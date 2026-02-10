40bc22a49cd6b43b32aae71b644fde19b1c7bc1e
ENG HASAN ALI <hasenali7770@gmail.com>
2026-02-10 12:23:47 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { typedRoutes: true },
+  // هذا السطر هو مفتاح الحل؛ سيجعل الروابط تعمل ببساطة كـ string
+  experimental: {
+    typedRoutes: false,
+  },
 };
+
 export default nextConfig;
