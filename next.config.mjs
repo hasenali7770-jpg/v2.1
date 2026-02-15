b1ad695347e7d0cd555cb4ce8191abaadb58d19e
ENG HASAN ALI <hasenali7770@gmail.com>
2026-02-15 23:57:37 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // هذا السطر هو مفتاح الحل؛ سيجعل الروابط تعمل ببساطة كـ string
-  experimental: {
-    typedRoutes: false,
-  },
-};
-
-export default nextConfig;
